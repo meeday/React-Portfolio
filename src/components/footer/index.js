7d4993d00b32534bc3e65d703fac92fd0d178d28
meeday <59993824+meeday@users.noreply.github.com>
2020-08-17 12:45:56 +0100

    footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styling.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p style={{color: "white", textAlign: "center", fontSize: "small"}} className="copyright-text">
+        Copyright &copy; 2020 All Rights Reserved by Meedaxa Ahmed
+      </p>
+    </footer>
+  );
+}

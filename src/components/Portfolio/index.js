e8f93e48418ec6ee8223c9d757ca97bce3c5a0bd
meeday <59993824+meeday@users.noreply.github.com>
2020-08-17 12:46:39 +0100

    component that holds all the other components

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../header/index";
+import Profile from "../profile/index";
+import "../../styling.css";
+
+export default function Portfolio() {
+  return (
+    <>
+      <Header />
+      <Profile />
+    </>
+  );
+}
